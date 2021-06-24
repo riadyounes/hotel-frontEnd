104cafbeded79c8172feee806a2061ce1beb1928
Riad Younes <50601899+riadyounes@users.noreply.github.com>
2021-06-24 19:33:53 -0300

    integracao como a api( hospedes ) (#1)

--- src/pages/hospedes/create.tsx
+++ src/pages/hospedes/create.tsx
@@ -16,37 +16,28 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
 import { useState } from "react";
+import { useCallback } from "react";
+import { api } from "../../services/api";
 
-type CreateHospedeFormData = {
-  name: string;
-  email: string;
-  cpf: string;
-  telefone: number;
-  dateNascimento: Date;
-  nacionalidade: string;
-};
+
 
 const CreateUserFormSchema = yup.object().shape({
-  name: yup.string().required("Nome obrigatório"),
+  nome: yup.string().required("Nome obrigatório"),
   email: yup
     .string()
     .required("E-mail obrigatório")
     .email("Digite um e-mail válido"),
-  password: yup
-    .string()
-    .required("Senha obrigatória")
-    .min(6, "No mínimo 6 caracteres"),
-  password_confirmation: yup
-    .string()
-    .oneOf([null, yup.ref("password")], "As senhas presisam ser iguais"),
+  cpf: yup.string().required("CPF obrigatório"),
+  nacionalidade: yup.string().required("Nacionalidade obrigatório"),
+  telefone: yup.number().required("Telefone obrigatório")
 });
 
 export default function CreateHospede() {
-  const [name, setName] = useState("");
+  const [nome, setName] = useState("");
   const [email, setEmail] = useState("");
   const [cpf, setCpf] = useState("");
   const [telefone, setTelefone] = useState(0);
-  const [dataNascimento, setDataNascimento] = useState("");
+  const [data_nascimento, setDataNascimento] = useState("");
   const [nacionalidade, setNacionalidade] = useState("");
 
   const { register, handleSubmit, formState } = useForm({
@@ -55,100 +46,113 @@ export default function CreateHospede() {
 
   const { errors } = formState;
 
-  const handleCreateHospede: SubmitHandler<CreateHospedeFormData> = async (
-    values
-  ) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(values);
-  };
+  const createHospede = useCallback(async (data) => {
+    
+    try {
+      await api.post("hospedes", data);
+      
+    } catch (error) {
+      console.log(error.error);
+    }
+  }, []);
+
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" mx="auto" maxWidth={1480} px="6">
         <SideBar />
         <Box
-          as="form"
+          // as="form"
           flex="1"
           borderRadius={8}
           bg="gray.800"
           p="8"
-          onSubmit={handleSubmit(handleCreateHospede)}
+          // onSubmit={createHospede}
         >
           <Heading fontSize="lg" fontWeight="normal">
             Criar hospede
           </Heading>
           <Divider my="6" borderColor="gray.700" />
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-              <Input
-                name="name"
-                label="Nome completo"
-                error={errors.name}
-                {...register("name")}
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              />
-              <Input
-                name="email"
-                type="email"
-                label="E-mail"
-                error={errors.email}
-                {...register("email")}
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-              <Input
-                name="cpf"
-                label="CPF"
-                {...register("cpf")}
-                value={cpf}
-                onChange={(event) => setCpf(event.target.value)}
-              />
-              <Input
-                name="telefone"
-                type="tel"
-                label="Telefone"
-                {...register("telefone")}
-                value={telefone}
-                onChange={(event) => setTelefone(Number(event.target.value))}
-              />
-            </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-              <Input
-                name="dataNascimento"
-                type="date"
-                label="Data de nascimento"
-                {...register("dataNascimento")}
-                value={dataNascimento}
-                onChange={(event) => setDataNascimento(event.target.value)}
-              />
-              <Input
-                name="nacionalidade"
-                label="Nacionalidade"
-                {...register("nacionalidade")}
-                value={nacionalidade}
-                onChange={(event) => setNacionalidade(event.target.value)}
-              />
-            </SimpleGrid>
-          </VStack>
-          <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
-              <Link href="/hospedes">
-                <Button as="a" colorScheme="whiteAlpha">
-                  Cancelar
+          <form onSubmit={handleSubmit(createHospede)}>
+            <VStack spacing="8">
+              <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+                <Input
+                  name="nome"
+                  label="Nome completo"
+                  error={errors.nome}
+                  {...register("nome")}
+                  value={nome}
+                  onChange={(event) => setName(event.target.value)}
+                />
+                <Input
+                  name="email"
+                  type="email"
+                  label="E-mail"
+                  error={errors.email}
+                  {...register("email")}
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </SimpleGrid>
+              <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+                <Input
+                  name="cpf"
+                  label="CPF"
+                  error={errors.cpf}
+                  {...register("cpf")}
+                  value={cpf}
+                  onChange={(event) => setCpf(event.target.value)}
+                />
+                <Input
+                  name="telefone"
+                  type="number"
+                  label="Telefone"
+                  error={errors.telefone}
+
+                  {...register("telefone")}
+                  
+                  onChange={(event) => setTelefone(Number(event.target.value))}
+                />
+              </SimpleGrid>
+              <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+                <Input
+                  name="data_nascimento"
+                  type="date"
+                  label="Data de nascimento"
+                  error={errors.data_nascimento}
+
+                  {...register("data_nascimento")}
+                  value={data_nascimento}
+                  onChange={(event) => setDataNascimento(event.target.value)}
+                />
+                <Input
+                  name="nacionalidade"
+                  label="Nacionalidade"
+                  error={errors.nacionalidade}
+
+                  {...register("nacionalidade")}
+                  value={nacionalidade}
+                  onChange={(event) => setNacionalidade(event.target.value)}
+                />
+              </SimpleGrid>
+            </VStack>
+            <Flex mt="8" justify="flex-end">
+              <HStack spacing="4">
+                <Link href="/hospedes">
+                  <Button as="a" colorScheme="whiteAlpha">
+                    Cancelar
+                  </Button>
+                </Link>
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  isLoading={formState.isSubmitting}
+                >
+                  Salvar
                 </Button>
-              </Link>
-              <Button
-                type="submit"
-                colorScheme="blue"
-                isLoading={formState.isSubmitting}
-              >
-                Salvar
-              </Button>
-            </HStack>
-          </Flex>
+              </HStack>
+            </Flex>
+          </form>
         </Box>
       </Flex>
     </Box>
