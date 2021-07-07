96b5d72dbe07dcce16ab899d6d61988035f44782
Riad Younes <riad.younes@hotmail.com>
2021-07-06 21:32:05 -0300

    conflito create reserva

--- src/pages/reservas/create.tsx
+++ src/pages/reservas/create.tsx
@@ -11,20 +11,16 @@ import {
 } from "@chakra-ui/react";
 
 import Link from "next/link";
+import { useCallback } from "react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
-
-
+import { api } from "../../services/api";
 
 export default function CreateReserva() {
   const createReserva = useCallback(async (data) => {
     try {
       await api.post("reservas", data);
-
-
-
-
     } catch (error) {
       console.log(error.error);
     }
@@ -41,9 +37,7 @@ export default function CreateReserva() {
           borderRadius={8}
           bg="gray.800"
           p="8"
-
           onSubmit={createReserva}
-
         >
           <Heading fontSize="lg" fontWeight="normal">
             Criar reserva
@@ -51,8 +45,6 @@ export default function CreateReserva() {
           <Divider my="6" borderColor="gray.700" />
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-
-              
               <Input name="data_entrada" label="Check-In" type="date" />
 
               <Input name="data_saida" label="Check-out" type="date" />
@@ -85,7 +77,6 @@ export default function CreateReserva() {
                 color="gray.900"
                 size="lg"
                 placeholder="Select country"
-
               >
                 <option>United Arab Emirates</option>
                 <option>Nigeria</option>
