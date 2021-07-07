e48ce728eb39fc7c22e02bb8fc60296c18b143a5
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-07-07 01:04:25 -0300

    refactor(pages): home page route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,15 @@
-import { Flex, Button, Stack } from "@chakra-ui/react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import* as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup"
-import { Input } from "../components/Form/Input";
+import { Flex } from "@chakra-ui/react";
 
-type SignInFormData = {
-  email: string;
-  password: string;
-};
+import { Header } from "../components/Header";
+import { SideBar } from "../components/SideBar";
 
-const signInFormSchema =yup.object().shape({
-  email: yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
-  password: yup.string().required('Senha obrigatória'),
-})
-
-export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(signInFormSchema)
-  });
-
-  const { errors } = formState;
-
-  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(values);
-  };
-
+export default function Dashboard() {
   return (
-    <Flex w="100wh" h="100vh" align="center" justify="center">
-      <Flex
-        as="form"
-        w="100%"
-        maxWidth={360}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDir="column"
-        onSubmit={handleSubmit(handleSignIn)}
-      >
-        <Stack spacing="4">
-          <Input
-            name="email"
-            type="email"
-            label="E-mail"
-            error={errors.email}
-            {...register("email", {required: 'Email obrigatorio'})}
-          />
-          <Input
-            name="password"
-            type="password"
-            label="Senha"
-            error={errors.password}
-            {...register("password")}
-          />
-        </Stack>
-        <Button
-          type="submit"
-          mt="6"
-          colorScheme="cyan"
-          size="lg"
-          isLoading={formState.isSubmitting}
-        >
-          Entrar
-        </Button>
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" mx="auto" maxWidth={1480} px="6">
+        <SideBar />
       </Flex>
     </Flex>
   );
